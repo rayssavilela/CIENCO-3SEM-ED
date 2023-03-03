c3a7a10d3f7cc39eafc4a2f9cf9222385befe78f
rayssavilela <rayssa2410@outlook.com>
2023-03-03 20:44:28 -0300

    Lista 1

--- Lista 1/ex 9 lista estrutura de dados/main.cpp
+++ Lista 1/ex 9 lista estrutura de dados/main.cpp
@@ -6,13 +6,13 @@ int main()
 {
     int numal;
     float altura;
-    float pessoa = 10;
-    float alturamaior;
-    float alturamenor;
-    int nummaior;
-    int nummenor;
+    int pessoa = 3;
+    float alturamaior = 0.00;
+    float alturamenor = 999;
+    int nummaior = 0;
+    int nummenor = 999;
 
-    for(int i = 0; i < pessoa; i++)
+    for(int i = 1; i < pessoa; i++)
     {
         cout << "Defina o numero do aluno: ";
         cin >> numal;
@@ -20,18 +20,18 @@ int main()
         cout << "Defina a altura do aluno: ";
         cin >> altura;
 
-        if(altura < 1.50)
-        {
-            alturamenor = altura;
-            nummenor = numal;
-        }
-        else if(altura > 1.90)
+        if(altura > alturamaior)
         {
             alturamaior = altura;
             nummaior = numal;
         }
+        if(altura < alturamenor)
+        {
+            alturamenor = altura;
+            nummenor = numal;
+        }
 
     }
-            cout << "O aluno mais baixo e o: " << nummenor << " e sua altura e de: " << alturamenor << "cm" << endl;
-            cout << "O aluno mais alto e o: " << nummaior << " e sua altura e de: " << alturamaior << "cm" << endl;
+            cout << "O aluno mais baixo e o: " << nummenor << " e sua altura e de: " << alturamenor << "m" << endl;
+            cout << "O aluno mais alto e o: " << nummaior << " e sua altura e de: " << alturamaior << "m" << endl;
 }
